a0b635f6b1d1b0afaacf67da1d45b5ce92f17332
micealcalumgannon <nitroexfusion@gmail.com>
2023-10-25 14:11:08 +0100

    added contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './Contact.module.css'
+
+export default function Contact() {
+  return (
+    <div>
+      {/* <Meta /> */}
+      <div>
+        <h1 className={styles.headerTitle}>ROB DIAMOND CONTACT</h1>
+      </div>
+
+      {/* BTN */}
+      <div className={styles.heroBtn}>
+        <Link href="tel:07976753254" className={styles.linkBTN}>CALL NOW</Link>
+      </div>
+      <div className={styles.heroBtn}>
+        <Link
+          href={"https://wa.me/447976753254?text=Welcome%20to%20RobDiamond"} 
+          className={styles.linkBTN}
+        >
+          WHATSAPP
+        </Link>
+      </div>
+      <div className={styles.heroBtnDesktop}>07976753254</div>
+    </div>
+  )
+}
