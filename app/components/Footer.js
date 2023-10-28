5e17b729680d64e4d7d0abf34414029d4468ab9b
MycallDev <mycalldev@localhost.localdomain>
2023-10-28 03:11:33 +0100

    added pages to footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,6 +6,13 @@ export default function Footer() {
   return (
     <main>
         <Image src={'/canary_wharf_footer.jpg'} className={styles.imageFooter} width={1456} height={816} alt={'picture of canary wharf'} quality={100} />
+
+        <div className={styles.containerFlex}>
+          <div className={styles.itemFlex}>FAQs</div>
+          <div className={styles.itemFlex}>T&Cs</div>
+          <div className={styles.itemFlex}>REVIEWS</div>
+        </div>
+        
         <div className={styles.addressContainer}>
             <div className={styles.addressTitle}>ADDRESS</div>
             <div className={styles.addressContent}>25 Cabot Square, Canary Wharf, London E1 44Q</div>
