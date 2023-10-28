e862af7c8b9d149bdff8d56b803dfc28b7dbbc74
MycallDev <mycalldev@localhost.localdomain>
2023-10-28 12:30:40 +0100

    added terms and conditions and returns page

---
+++ app/return/page.jsx
@@ -0,0 +1,16 @@
+import styles from './Return.module.css'
+
+export default function Return() {
+  return (
+    <main>
+        <div className={styles.titleReturns}>
+        <h1>Returns Policy</h1>
+      </div>
+      <div className={styles.container}>
+      For Non-Faulty Items: We want you to be fully satisfied with your purchase, so we offer a 14-day return policy for non-faulty items. If you are not satisfied with your purchase, you may return the item for a full refund, excluding shipping costs. The item must be in its original condition, with all the accessories and packaging, and not worn or altered in any way. Please contact us to initiate the return process.
+
+      For Faulty Items: If you receive a faulty item, please contact us immediately. We will assess the issue and determine the best course of action. In most cases, we will offer a full refund or a replacement item. We will also cover the cost of shipping for the return of the faulty item.
+      </div>
+    </main>
+  )
+}
--- app/terms-and-conditions/page.jsx
+++ app/terms-and-conditions/page.jsx
@@ -6,7 +6,119 @@ export default function TermsAndConditions() {
 
   return (
     <div>
-        <h1 className={styles.titleMain}>THIS IS THE T&Cs PAGE</h1>
+      <h1 className={styles.titleMain}>T&Cs</h1>
+
+      
+
+      <div className={styles.titleTerms}> 
+        <h3>TERMS & CONDITIONS</h3>
+      </div>
+
+      <div>
+        <ol className={styles.orderedList}>
+          <h3 className={styles.listItem}>Introduction:</h3>
+          <li className={styles.listItem}>Welcome to Luxury Times Ltd, the online store for pre-owned Rolex watches. These terms and conditions govern your use of Luxury Times Ltd, our website, and your purchase of our products. By accessing or using Luxury Times Ltd website, you agree to be bound by these terms and conditions. If you do not agree to Luxury Times Ltd terms and conditions, please do not use our website.</li>
+          <li className={styles.listItem}>We sell pre-owned Rolex watches that have been carefully inspected and authenticated by Luxury Times Ltd team of experts. While we make every effort to accurately describe our products, there may be variations due to the age and condition of the watch. All our watches come with a guarantee of authenticity.</li>
+          <h3 className={styles.listItem}>Orders:</h3>
+          <li className={styles.listItem}>When you place an order with Luxury Times Ltd you are making an offer to purchase the product(s) in your order. We reserve the right to refuse or cancel any order for any reason, including but not limited to availability, pricing errors, or suspected fraud. We may also require additional verification or information before accepting an order.</li>
+          <h3 className={styles.listItem}>Pricing and Payment:</h3>
+          <li className={styles.listItem}>All prices listed on Luxury Times Ltd website are in Pound Sterling and are subject to change without notice. We accept payment by credit card, PayPal, or wire transfer. Payment must be made in full at the time of purchase.</li>
+          <h3 className={styles.listItem}>Shipping and Delivery:</h3>
+          <li className={styles.listItem}>Luxury Times Ltd offer shipping worldwide for all orders. Delivery times may vary depending on the destination, and we cannot be held responsible for any customs fees or taxes imposed by the recipient's country.
+                                           Returns and Refunds:</li>
+          <li className={styles.listItem}>Luxury Times Ltd offer a 14-day return policy for non-faulty items. If you are not satisfied with your purchase, you may return the item for a full refund, excluding shipping costs. The item must be in its original condition, with all the accessories and packaging, and not worn or altered in any way. If you receive a faulty item, please contact Luxury Times Ltd immediately. We will assess the issue and determine the best course of action, Luxury Times Ltd may include a full refund or replacement item.</li>
+          <h3 className={styles.listItem}>Warranty:</h3>
+          <li className={styles.listItem}>All our watches come with a 12-month warranty covering any defects in materials or workmanship. The warranty does not cover normal wear and tear or damage caused by accidents, misuse, or neglect.</li>
+          <h3 className={styles.listItem}>Intellectual Property:</h3>
+          <li className={styles.listItem}>All content on our website, including but not limited to text, images, logos, and trademarks, is the property of Luxury Times Ltd and is protected by copyright and other intellectual property laws. You may not use, reproduce, or distribute any content from our website without our prior written consent.</li>
+          <h3 className={styles.listItem}>Privacy:</h3>
+          <li className={styles.listItem}>Luxury Times Ltd take your privacy seriously and will only use your personal information to fulfil your order and communicate with you about our products and services. Luxury Times Ltd will not sell or share your information with third parties except as necessary to process your order or comply with legal obligations.</li>
+          <h3 className={styles.listItem}>Governing Law:</h3>
+          <li className={styles.listItem}>These terms and conditions shall be governed by and construed in accordance with the laws of London, United Kingdom . Any dispute arising out of or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the courts of London UK. </li>
+        </ol>
+      </div>
+
+      <div className={styles.privacyNotice}>
+        <h3>Privacy Notice for luxurytimesltd Website Pre-Owned Rolex Watches: www.luxurytimesltd.co.uk</h3>
+      </div>
+
+      <div className={styles.additional}>
+        <p>At Luxury Times Ltd, we understand the importance of protecting 
+          your personal information and privacy. 
+          This Privacy Notice outlines our policies 
+          and procedures for collecting, using, 
+          and disclosing your personal information 
+          when you visit our website to purchase pre-owned Rolex watches.
+        </p>
+        
+        <h3>Information We Collect:</h3>
+
+        <p>Luxury Times Ltd collect personal information that you 
+          provide to us when you register an account with us,
+          make a purchase, or contact our customer service team. 
+          This may include your name, email address, phone number, 
+          and shipping address. Luxury Times Ltd may also collect information 
+          about your browsing behaviour on our website, 
+          including the pages you visit and the links you click.
+        </p>
+
+        <h3>How Luxury Times Ltd Use Your Information:</h3> 
+
+        <p>We use your personal information to process your orders, 
+        deliver your purchases, and communicate with you about your 
+        account or any issues related to your purchase. 
+        Luxury Times Ltd may also use your information to send you promotional materials, 
+        newsletters, and other marketing communications.
+        </p>
+
+        <h3>Disclosure of Your Information:</h3> 
+
+        <p>Luxury Times Ltd do not share, sell, or rent your personal 
+        information with third parties for their marketing purposes. 
+        However, Luxury Times Ltd may share your information with 
+        third-party service providers who assist us in delivering 
+        our services to you, such as shipping companies or payment processors. 
+        Luxury Times Ltd may also disclose your information if 
+        required by law or if necessary to protect our rights or the rights of others.
+        </p>
+
+        <h3>Security of Your Information:</h3>
+
+        <p>Luxury Times Ltd take reasonable steps to ensure the security 
+          of your personal information by implementing appropriate physical, 
+          electronic, and managerial procedures to safeguard against 
+          unauthorised access, disclosure, or use. 
+          However, no data transmission over the internet can be 
+          guaranteed to be 100% secure, so Luxury Times Ltd cannot 
+          guarantee the security of any information you provide to us.
+        </p>
+
+        <h3>Your Rights:</h3>
+
+        <p>You have the right to access, correct, or delete your personal 
+          information that we hold. You may also object to the processing 
+          of your personal information, restrict Luxury Times Ltd use of 
+          your information, or request that Luxury Times Ltd transfer your 
+          information to another controller. To exercise any of these rights, 
+          please contact Luxury Times Ltd using the information provided below.
+        </p>
+
+        <h3>Changes to This Notice:</h3> 
+
+        <p>Luxury Times Ltd may update this Privacy Notice 
+          from time to time to reflect changes in our practices or legal obligations. 
+          Luxury Times Ltd encourage you to review this notice periodically 
+          to stay informed of our privacy practices.
+        </p>
+
+        <h3>Contact Us:</h3>
+
+      <p>If you have any questions or concerns about our Privacy Notice, 
+        or if you wish to exercise your rights regarding your personal information, 
+        please contact us via our contact page on the 
+        website: www.luxurytimesltd.co.uk/contact.
+      </p>
+      </div>
     </div>
   )
 }
