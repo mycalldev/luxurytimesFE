3a15802152bfcaf102c5f6bb23f3dd77f2d76677
MycallDev <mycalldev@localhost.localdomain>
2023-10-29 11:36:37 +0000

    created Patek Philippe Sub-category pages

---
+++ app/patek-philippe-categories/grand-complication/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Aquanaut() {
+  return (
+    <div>This is The Grand-Complication Sub-Category Page</div>
+  )
+}
---
+++ app/patek-philippe-categories/nautilus/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Aquanaut() {
+  return (
+    <div>This is The Nautilus Sub-Category Page</div>
+  )
+}
