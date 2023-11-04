ce22b702a9ea5e379f8addec33819256c43ca66a
micealcalumgannon <nitroexfusion@gmail.com>
2023-11-04 18:56:54 +0000

    created burger dropdown for mobile

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,38 +1,89 @@
-import React from 'react'
+'use client'
+import React, {useState} from 'react'
 import styles from './Navbar.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import BurgerMenu from './BurgerMenu'
 
 export default function Navbar() {
+
+  const [toggle, setToggle] = useState(false)
+
+  const handleToggle = () => {
+      setToggle(!toggle)
+  }
+
   return (
-    <div className={styles.containerFlex}>
-      <div>
-        <Image
-          src={'/hamburger.png'}
-          width={32}
-          height={32}
-          alt={'menu bar'}
-          quality={100}
-        />
-      </div>
-
-      <div>
-        <Link href="/#" className={styles.undreline}>
-          <div className={styles.titleMain}> LUXURY TIMES</div>
-        </Link>
-      </div>
-
-      <div className={styles.contactIcon}>
-        <Link href="tel:07976753254">
-          <Image
-            src={'/phone_icon.png'}
+    <main>
+      <div className={styles.navContainer}>
+        <div className={styles.containerFlex}>
+          <div className={styles.imageBurger} onClick={() => handleToggle()}>
+            <Image
+            src={'/hamburger.png'}
             width={32}
             height={32}
-            alt={'contact button'}
+            alt={'menu bar'}
             quality={100}
-          />
-        </Link>
+            />
+          </div>
+          
+          <div>
+            <Link href="/#" className={styles.undreline}>
+              <div className={styles.titleMain}>LUXURY TIMES</div>
+            </Link>
+          </div>
+
+          <div className={styles.contactIcon}>
+            <Link href="tel:07976753254">
+              <Image
+                src={'/phone_icon.png'}
+                width={32}
+                height={32}
+                alt={'contact button'}
+                quality={100}
+              />
+            </Link>
+          </div>
       </div>
     </div>
+    {toggle ? <nav className={styles.navOpen}>
+      <ul className={styles.unorderedList}>
+        <Link href={'/rolex/rolex-categories'} className={styles.linkBTN}>
+          <div className={styles.listItemContainer} onClick={() => handleToggle()}>
+            <li className={styles.listItem}>Rolex</li>
+          </div>
+        </Link>
+        <Link href={'/patek-philippe-categories'} className={styles.linkBTN}>
+          <div className={styles.listItemContainer} onClick={() => handleToggle()}>
+            <li className={styles.listItem}>Patek Philippe</li>
+          </div>
+        </Link>
+        <Link href={'/audemars-piguet-categories'} className={styles.linkBTN}>
+          <div className={styles.listItemContainer} onClick={() => handleToggle()}>
+            <li className={styles.listItem}>Audemars Piguet</li>
+          </div>
+        </Link>
+        <Link href={'/guide'} className={styles.linkBTN}>
+          <div className={styles.listItemContainer} onClick={() => handleToggle()}>
+            <li className={styles.listItem}>Guides</li>
+          </div>
+        </Link>
+        <Link href={'/blog'} className={styles.linkBTN}>
+          <div className={styles.listItemContainer} onClick={() => handleToggle()}>
+            <li className={styles.listItem}>Blogs</li>
+          </div>
+        </Link>
+        <Link href={'/contact'} className={styles.linkBTN}>
+          <div className={styles.listItemContainer} onClick={() => handleToggle()}>
+            <li className={styles.listItem}>Contact</li>
+          </div>
+        </Link>
+      </ul>
+      <div className={styles.closeMenu} onClick={() => handleToggle()}>Close Menu</div>
+    </nav> : 
+    <div></div>
+    }
+    
+    </main>
   )
 }
