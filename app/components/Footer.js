7469b0c2cead7eb0222e3ab21b1077d1f744fb39
MycallDev <mycalldev@localhost.localdomain>
2023-11-05 22:51:54 +0000

    added links props to footer links

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './Footer.module.css'
 
 export default function Footer() {
@@ -8,9 +9,15 @@ export default function Footer() {
         <Image src={'/canary_wharf_footer.jpg'} className={styles.imageFooter} width={1456} height={816} alt={'picture of canary wharf'} quality={100} />
 
         <div className={styles.containerFlex}>
-          <div className={styles.itemFlex}>FAQs</div>
-          <div className={styles.itemFlex}>T&Cs</div>
-          <div className={styles.itemFlex}>REVIEWS</div>
+          <Link href={'/FAQs'} className={styles.linkBTN}>
+            <div className={styles.itemFlex}>FAQs</div>
+          </Link>
+          <Link href={'/terms-and-conditions'} className={styles.linkBTN}>
+            <div className={styles.itemFlex}>T&Cs</div>
+          </Link>
+          <Link href={'/review'} className={styles.linkBTN}>
+            <div className={styles.itemFlex}>REVIEWS</div>
+          </Link>
         </div>
         
         <div className={styles.addressContainer}>
