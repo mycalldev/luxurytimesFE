fc7ef943b47dbce8dbd1be4f553f5351d9d54cba
mycallCampbell <mycallcampbell@gmail.com>
2024-07-29 12:52:18 +0100

    updated css on 404 page

--- app/not-found.js
+++ app/not-found.js
@@ -8,8 +8,8 @@ export default function notFound() {
         <h1 className={styles.big404}>404</h1>
         <p className={styles.textNotFound}>We could not find the page you were looking for.</p>
         <p className={styles.textNotFound}>Return to the</p>
-        <div className={styles.contactBTNContainerMobile}>
-        <Link href="/" className={styles.linkBTN} >
+        <div>
+        <Link href="/" className={styles.link404} >
             <div className={styles.viewDetailsBTN}>HOME PAGE</div>
         </Link>
       </div>
