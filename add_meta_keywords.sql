c7cd060e326214217a85cd470dc9bfa0683c02b2
mycall <devmycall@gmail.com>
2025-03-15 11:37:02 +0000

    removed large version of sub blueberry

--- add_meta_keywords.sql
+++
@@ -1,8 +0,0 @@
--- Add meta_keywords column to the blogs table if it doesn't exist
-ALTER TABLE blogs ADD COLUMN IF NOT EXISTS meta_keywords TEXT;
-
--- Verify the column was added
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'blogs'
-ORDER BY ordinal_position; 
--- app/admin/auth-test/metadata.js
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: 'Authentication Test | Luxury Times Admin',
-  description: 'Authentication testing page for Luxury Times admin dashboard'
-}; 
