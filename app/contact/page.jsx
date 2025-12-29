17e3d42e81361ba4c57c9ed2f549245d668f0623
Michael <mycallcampbell@gmail.com>
2025-12-29 16:58:21 +0000

    updated the contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,27 +1,43 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from './Contact.module.css'
 import Button from '../components/Button'
 
 export default function Contact() {
   return (
-    <div>
-      {/* <Meta /> */}
-      <div>
-        <h1 className={styles.headerTitle}>LUXURY TIMES CONTACT</h1>
+    <main>
+      <Link href={'/'} className={styles.backLinkBTN}>
+        <div className={styles.backNav}>
+          {'< Home'}
+        </div>
+      </Link>
+      
+      <div className={styles.container}>
+        <div className={styles.buttonWrapper}>
+          <Button href="tel:07714611699" desktopText="07714611699">
+            CONTACT
+          </Button>
+        </div>
+
+        <div className={styles.titleMain}>
+          Get in Touch with Luxury Times
+        </div>
+
+        <div className={styles.contentExtra}>
+          Luxury Times Ltd is a Family Run Business. We're here to help and answer any questions you might have. 
+          Whether you have a query about our products or services, 
+          want to provide feedback, or simply want to say hello, we'd love to hear from you.
+        </div>
+
+        <div className={styles.contentExtra}>
+          Please feel free to reach out to us using the contact button above, 
+          and we'll get back to you as soon as possible.
+        </div>
+
+        <div className={styles.contentExtraThick}>
+          We look forward to hearing from you and helping you with all your luxury timepiece needs.
+        </div>
       </div>
-
-      <div className={styles.contactText}>
-        Luxury Times Ltd is a Family Run Business. We're here to help and answer any questions you might have. 
-        Whether you have a query about our products or services, 
-        want to provide feedback, or simply want to say hello, we'd love to hear from you. 
-        Please feel free to reach out to us using the contact information below, 
-        and we'll get back to you as soon as possible.
-      </div>
-
-
-      <Button href="tel:07714611699" desktopText="07714611699">
-        CONTACT
-      </Button>
-    </div>
+    </main>
   )
 }
