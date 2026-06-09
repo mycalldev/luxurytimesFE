2a77182dce2e90d55588555a2a6372466a94faba
mycalldev <devmycall@gmail.com>
2026-06-09 12:09:27 +0100

    removed critters

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     qualities: [75, 80, 85, 90, 100],
     remotePatterns: [
